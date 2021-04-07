afb258db743861db0710a0fc2ad0d5a04c8a5793
Wilbur Huang <wilbur@kearnstechnology.com>
2021-04-07 14:41:24 -0400

    multple myeloma page first pass

--- src/js/components/hematology/research/MultipleMyeloma.js
+++ src/js/components/hematology/research/MultipleMyeloma.js
@@ -15,7 +15,7 @@ const MultipleMyeloma = () =>
   }, [ updateMetaTitle, updateMetaDescription ]);
 
   return (
-    <div className="hematology-our-research-wrapper"  style={{ backgroundImage: "url(/img/hematology/research/background-hematology-research-lymphoma.png)" }}>
+    <div className="hematology-our-research-wrapper"  style={{ backgroundImage: "url(/img/hematology/research/background-multiple-myeloma.jpg)" }}>
       <div className="hematology-our-research-header">
         <Link to="/" className="logo-wrapper"><img src={ process.env.PUBLIC_URL + "/img/logo.png" } alt="BMS logo" /></Link>
       </div>
@@ -25,37 +25,35 @@ const MultipleMyeloma = () =>
           <div className="main-area-wrapper-inner">
             <h1>Multiple Myeloma</h1>
 
-            <div className="myelofibrosis-mechanism link-wrapper">
-              <img alt="Myelofibrosis mechanism of disease " className="myelofibrosis-mechanism-img" src="/img/hematology/research/myelofibrosis-mechanism-of-disease.png"></img>
-              <h2>Myelofibrosis mechanism of disease (video)</h2>
+            <div className="targeting-protein-wrapper link-wrapper">
+              <img alt="Targeting protein degradation through cereblon modulation" className="targeting-protein-img" src="/img/hematology/research/targeting-protein-degradation-through-cereblon-modulation.png"></img>
+              <h2>Targeting protein degradation through cereblon modulation</h2>
             </div>
 
-            <div className="additional-clinical-trials-wrapper">
+            <div className="multiple-myeloma-additional-clinical-trials-wrapper">
               <span>Additional Clinical Trials</span>
               <a href="https://www.bolderscience.com/bmspipeline/solidtumorhematology/resources/event-3/" target="_blank" rel="noreferrer" className="pink-circle"><FontAwesomeIcon icon={ faPlus } /></a>
             </div>
 
-            <div className="myeloid-scientific-resources-wrapper">
+            <div className="multiple-myeloma-scientific-resources-wrapper">
               <span>Scientific Resources</span>
               <a href="https://www.bolderscience.com/bmspipeline/solidtumorhematology/resources/event-3/" target="_blank" rel="noreferrer" className="pink-circle"><FontAwesomeIcon icon={ faPlus } /></a>
             </div>
 
-            <div className="fedratinib-wrapper link-wrapper">
-              <img alt="Clinical trial: Fedratinib in myelofibrosis" className="fedratinib-img" src="/img/hematology/research/clinical-trial-fedratinib-in-myelofibrosis.png"></img>
-              <h2>Clinical trial: Fedratinib in myelofibrosis</h2>
+            <div className="cereblon-modulation-in-multiple-myeloma-wrapper link-wrapper">
+              <img alt="Cereblon modulation in multiple myeloma" className="cereblon-modulation-in-multiple-myeloma-img" src="/img/hematology/research/Cereblon-modulation-in multiple-myeloma.png"></img>
+              <h2>Cereblon modulation in multiple myeloma</h2>
             </div>
 
-            <div className="luspatercept-erythropoiesis-wrapper link-wrapper">
-              <img alt="Clinical trial: Luspatercept in erythropoiesis disorders" className="luspatercept-erythropoiesis-img" src="/img/hematology/research/clinical-trial-luspatercept-in-erythropoiesis-disorders.png"></img>
-              <h2>Clinical trial: Luspatercept in erythropoiesis disorders</h2>
+            <div className="targeting-BCMA-in-multiple-myeloma-wrapper link-wrapper">
+              <img alt="Targeting BCMA in multiple myeloma" className="targeting-BCMA-in-multiple-myeloma-img" src="/img/hematology/research/Targeting-BCMA-in-multiple-myeloma.png"></img>
+              <h2>Targeting BCMA in multiple myeloma</h2>
             </div>
 
-            <div className="luspatercept-mpn-wrapper link-wrapper">
-              <img alt="Clinical trial: Luspatercept in MPN-associated myelofibrosis" className="luspatercept-mpn-img" src="/img/hematology/research/clinical-trial-luspatercept-in-MPN-associated-myelofibrosis.png"></img>
-              <h2>Clinical trial: Luspatercept in MPN-associated myelofibrosis</h2>
+            <div className="clinical-trial-Idecabtagene-vicleucel-in-multiple-myeloma-wrapper link-wrapper">
+              <img alt="Clinical trial: Idecabtagene vicleucel in multiple myeloma" className="clinical-trial-Idecabtagene-vicleucel-in-multiple-myeloma-img" src="/img/hematology/research/Clinical-trial-Idecabtagene-vicleucel-in-multiple-myeloma.jpg"></img>
+              <h2>Clinical trial: Idecabtagene vicleucel in multiple myeloma</h2>
             </div>
-
-
 
             <div className="medical-information-wrapper">
               <span>Medical Information</span>
