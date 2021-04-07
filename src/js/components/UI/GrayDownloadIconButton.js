821136af915d89ca9397a36b196939fffcfa2a40
Artem Loshkov <artem@kearnstechnology.com>
2021-04-07 16:01:29 -0400

    Much more WIP

--- src/js/components/UI/GrayDownloadIconButton.js
+++ src/js/components/UI/GrayDownloadIconButton.js
@@ -3,7 +3,7 @@ import React from "react";
 const GrayDownloadIconButton = (props) =>
 {
   return (
-    <a className="gray-download-icon-button" href={ props.link } target={ props.blank ? "_blank" : "_self" } rel="noreferrer">
+    <a className={ "gray-download-icon-button" + (props.className ? (" " + props.className) : "") } href={ props.link } target={ props.blank ? "_blank" : "_self" } rel="noreferrer" onClick={ props.onClick }>
       { props.name }
       <img src={ process.env.PUBLIC_URL + "/img/icons/icon-download.svg" } alt="Download" />
     </a>
--- src/js/components/hematology/OurMedicines.js
+++ src/js/components/hematology/OurMedicines.js
@@ -41,16 +41,16 @@ const OurMidicines = () =>
             </div>
 
             <div className="abecma-wrapper link-wrapper" onClick={ () => setModalName("abecma-general") }></div>
-            <div className="breyanzi-wrapper link-wrapper" onClick={ () => setModalName("breyanzi-general")}></div>
-            <div className="reblozyl-wrapper link-wrapper" onClick={ () => setModalName("reblozyl-general")}></div>
-            <div className="onureg-wrapper link-wrapper" onClick={ () => setModalName("onureg-general")}></div>
-            <div className="revlimid-wrapper link-wrapper" onClick={ () => setModalName("revlimid-general")}></div>
-            <div className="pomalyst-wrapper link-wrapper" onClick={ () => setModalName("pomalyst-general")}></div>
-            <div className="empliciti-wrapper link-wrapper" onClick={ () => setModalName("empliciti-general")}></div>
-            <div className="r2-wrapper link-wrapper" onClick={ () => setModalName("r2-general")}></div>
-            <div className="inrebic-wrapper link-wrapper" onClick={ () => setModalName("inrebic-general")}></div>
-            <div className="sprycel-wrapper link-wrapper" onClick={ () => setModalName("sprycel-general")}></div>
-            <div className="idhifa-wrapper link-wrapper" onClick={ () => setModalName("idhifa-general")}></div>
+            <div className="breyanzi-wrapper link-wrapper" onClick={ () => setModalName("breyanzi-general") }></div>
+            <div className="reblozyl-wrapper link-wrapper" onClick={ () => setModalName("reblozyl-general") }></div>
+            <div className="onureg-wrapper link-wrapper" onClick={ () => setModalName("onureg-general") }></div>
+            <div className="revlimid-wrapper link-wrapper" onClick={ () => setModalName("revlimid-general") }></div>
+            <div className="pomalyst-wrapper link-wrapper" onClick={ () => setModalName("pomalyst-general") }></div>
+            <div className="empliciti-wrapper link-wrapper" onClick={ () => setModalName("empliciti-general") }></div>
+            <div className="r2-wrapper link-wrapper" onClick={ () => setModalName("r2-general") }></div>
+            <div className="inrebic-wrapper link-wrapper" onClick={ () => setModalName("inrebic-general") }></div>
+            <div className="sprycel-wrapper link-wrapper" onClick={ () => setModalName("sprycel-general") }></div>
+            <div className="idhifa-wrapper link-wrapper" onClick={ () => setModalName("idhifa-general") }></div>
           </div>
         </div>
       </section>
@@ -63,7 +63,7 @@ const OurMidicines = () =>
         <a href="https://www.bms.com/privacy-policy.html" target="_blank" rel="noreferrer" className="button-dark privacy-policy-btn">Privacy Policy</a>
       </div>
 
-      { modalName &&
+      { !!modalName &&
         <Modal
           show={ !!modalName }
           onHide={ () => setModalName("") }
@@ -93,7 +93,7 @@ const OurMidicines = () =>
 
             { modalName === "breyanzi-general" &&
               <div className="general-section-wrapper">
-                <a href="PI:https://packageinserts.bms.com/pi/pi_breyanzi.pdf?#view=Fit&toolbar=0" target="_blank" rel="noreferrer"><img src={ process.env.PUBLIC_URL + "/img/hematology/our-medicines/breyanzi.png" } alt="Abecma" /></a>
+                <a href="https://packageinserts.bms.com/pi/pi_breyanzi.pdf?#view=Fit&toolbar=0" target="_blank" rel="noreferrer"><img src={ process.env.PUBLIC_URL + "/img/hematology/our-medicines/breyanzi.png" } alt="Breyanzi" /></a>
                 <Link to="#" onClick={ () => setModalName("breyanzi-detailed") } className="button-pink">Learn More</Link>
               </div>
             }
@@ -106,6 +106,41 @@ const OurMidicines = () =>
                 <PinkLinkIconButton name="BREYANZI Website" link="https://www.breyanzihcp.com/" blank={ true } />
                 <p style={{ margin: "3rem 0 0.5rem 0", fontWeight: "700" }}>Explore Additional Information:</p>
                 <GrayDownloadIconButton name="BREYANZI Full Prescribing Information" link="https://packageinserts.bms.com/pi/pi_breyanzi.pdf?#view=Fit&toolbar=0" blank={ true } />
+              </div>
+            }
+
+            { modalName === "reblozyl-general" &&
+              <div className="general-section-wrapper">
+                <a href="https://packageinserts.bms.com/pi/pi_reblozyl.pdf?#view=Fit&toolbar=0" target="_blank" rel="noreferrer"><img src={ process.env.PUBLIC_URL + "/img/hematology/our-medicines/reblozyl.png" } alt="Reblozyl" /></a>
+                <Link to="#" onClick={ () => setModalName("reblozyl-detailed") } className="button-pink">Learn More</Link>
+              </div>
+            }
+
+            { modalName === "reblozyl-detailed" &&
+              <div className="detailed-section-wrapper">
+                <img src={ process.env.PUBLIC_URL + "/img/hematology/our-medicines/reblozyl-details-page.png" } alt="Breyanzi" />
+                <hr />
+                <p style={{ maxWidth: "500px", marginBottom: "3rem" }}>Please see U.S. Full Prescribing information for REBLOZYL<sup>&reg;</sup> (luspatercept-aamt) available in the link below.
+                </p>
+                <PinkLinkIconButton name="REBLOZYL Website" link="https://www.reblozylpro.com/" blank={ true } />
+                <p style={{ margin: "3rem 0 0.5rem 0", fontWeight: "700" }}>Explore Additional Information:</p>
+                <GrayDownloadIconButton name="REBLOZYL Full Prescribing Information" link="https://packageinserts.bms.com/pi/pi_reblozyl.pdf?#view=Fit&toolbar=0" blank={ true } className="reblozyl-details-link" />
+                <GrayDownloadIconButton name="REBLOZYL Second Indication Product Brochure" link={ process.env.PUBLIC_URL + "/pdf/2007-US-2100054_cellus_mds_hcp_bro_update.pdf?#view=Fit&toolbar=0" } blank={ true } className="reblozyl-details-link" />
+                <GrayDownloadIconButton name="REBLOZYL First Indication Product Brochure" link={ process.env.PUBLIC_URL + "/pdf/2007-US-2100003 Beta Thal Shortened Brochure_DIGITAL.pdf?#view=Fit&toolbar=0" } blank={ true } className="reblozyl-details-link" />
+                <GrayDownloadIconButton name="REBLOZYL Dosing & Reconstitution Guide" link="https://gc-static-content.s3.amazonaws.com/bms/ash/reblozyl/US-RBZ-20-0290%20Dual%20Indication%20Dosing%20Guide%20%28Downloadable%29_Clickable_7.16.pdf?#view=Fit&toolbar=0" blank={ true } className="reblozyl-details-link" />
+                <GrayDownloadIconButton name="REBLOZYL First Indication Patient Brochure" link="https://gc-static-content.s3.amazonaws.com/bms/ash/reblozyl/US-RBZ-20-0303%20First%20Indication_BT_Patient%20Brochure%20V2%20Digital.pdf?#view=Fit&toolbar=0" blank={ true } className="reblozyl-details-link" />
+                <GrayDownloadIconButton name="REBLOZYL Second Indication Patient Brochure" link="https://gc-static-content.s3.amazonaws.com/bms/ash/reblozyl/US-RBZ-20-0232%2BMDS%2BPatient%2BBrochure%2B%28Digital%29_FinalClickable6.26.pdf?#view=Fit&toolbar=0" blank={ true } className="reblozyl-details-link" />
+                <GrayDownloadIconButton name="REBLOZYL MOA Video" onClick={ () => setModalName("reblozyl-moa-video") } className="reblozyl-details-link" />
+                <GrayDownloadIconButton name="REBLOZYL First Indication Dosing & Administration Video" onClick={ () => setModalName("reblozyl-first-indication-bosing-and-administration-video") } className="reblozyl-details-link" />
+                <GrayDownloadIconButton name="REBLOZYL Second Indication Dosing & Administration Video" onClick={ () => setModalName("reblozyl-second-indication-bosing-and-administration-video") } className="reblozyl-details-link" />
+              </div>
+            }
+
+            { modalName === "reblozyl-moa-video" &&
+              <div className="video-section-wrapper">
+                <video controls poster={ process.env.PUBLIC_URL + "/img/video-banner.png" }>
+                  <source src="https://virtual-video-resources.s3.amazonaws.com/bms/videos/US-RBZ-20-0443+MOA_HCP+GPO+Video+Asset+V2.mp4" type="video/mp4" />
+                </video>
               </div>
             }
 
