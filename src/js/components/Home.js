5416203e2366aaf28e1906fc9d8026099f4a1d69
Artem Loshkov <artem@kearnstechnology.com>
2021-05-05 15:00:26 -0400

    More link updates

--- src/js/components/Home.js
+++ src/js/components/Home.js
@@ -85,8 +85,8 @@ const Home = () =>
             <h3>Oncology</h3>
             <img src={ process.env.PUBLIC_URL + "/img/icons/oncology.svg" } alt="Oncology" />
             <ul className="homepage-card-links">
-              <li><a href="https://ourmedhq1.explorebmsvirtual.com/?utm_source=HO&utm_medium=OM_Oncology" target="_blank" rel="noreferrer" className="arrow-link-pink">Our Medicines <img src={ process.env.PUBLIC_URL + "/img/icons/arrow-right.svg" } alt="Arrow Right" /></a></li>
-              <li><a href="https://ourdisoq1.explorebmsvirtual.com/?utm_source=HO&utm_medium=DR_Oncology" target="_blank" rel="noreferrer" className="arrow-link-pink">Our Educational Resources <img src={ process.env.PUBLIC_URL + "/img/icons/arrow-right.svg" } alt="Arrow Right" /></a></li>
+              <li><a href="https://ourmedoq1.explorebmsvirtual.com/?utm_source=HO&utm_medium=OurMed_Oncology" target="_blank" rel="noreferrer" className="arrow-link-pink">Our Medicines <img src={ process.env.PUBLIC_URL + "/img/icons/arrow-right.svg" } alt="Arrow Right" /></a></li>
+              <li><a href="https://ourdisoq1.explorebmsvirtual.com/?utm_source=HO&utm_medium=Disease_Oncology" target="_blank" rel="noreferrer" className="arrow-link-pink">Our Educational Resources <img src={ process.env.PUBLIC_URL + "/img/icons/arrow-right.svg" } alt="Arrow Right" /></a></li>
             </ul>
           </div>
 
